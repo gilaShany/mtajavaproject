7ab8f173441edd65ed8700df18b527307ae35566
Shany680 <Shany@Shany-PC.home>
2015-12-16 16:16:16 +0200

    /

--- src/com/myorg/javacourse/service/PortfolioManager.java
+++ src/com/myorg/javacourse/service/PortfolioManager.java
@@ -16,35 +16,6 @@ public class PortfolioManager {
 	 * @return portfolio
 	 */
 	public Portfolio getPortfolio() {
-		/*Portfolio portfolio = new Portfolio();
-		portfolio.setTitle("Portfolio #1");
-		
-		Stock stock1 = new Stock();
-		Stock stock2 = new Stock();
-		Stock stock3 = new Stock();
-		
-		Date date1 = new Date(114, 10, 15);
-		Date date2 = new Date (114,10,15);
-		Date date3 = new Date (114,10,15);
-		
-		stock1.setSymbol("PIH");
-		stock1.setAsk((float) 13.1);
-		stock1.setBid((float) 12.4);
-		stock1.setDate(date1);
-		portfolio.addStock(stock1);
-		
-		stock2.setSymbol("ALL");
-		stock2.setAsk((float) 5.78);
-		stock2.setBid((float) 5.5);
-		stock2.setDate(date2);
-		portfolio.addStock(stock2);
-		
-		stock3.setSymbol("CAAS");
-		stock3.setAsk((float) 32.2);
-		stock3.setBid((float) 31.5);
-		stock3.setDate(date3);
-		portfolio.addStock(stock3); */
-		
 		Portfolio myPortfolio = new Portfolio();
 		myPortfolio.setTitle("Exercise 7 portfolio");
 		myPortfolio.updateBalance(10000f);
--- src/com/myorg/javacourse/servlet/PortfolioServlet.java
+++ src/com/myorg/javacourse/servlet/PortfolioServlet.java
@@ -18,24 +18,6 @@ public class PortfolioServlet extends HttpServlet {
 		PortfolioManager portfolioManager = new PortfolioManager();
 		Portfolio portfolio1 = portfolioManager.getPortfolio();
 		resp.getWriter().println(portfolio1.getHtmlString());
-		/*
-		Portfolio portfolio2 = new Portfolio (portfolio1);
-		portfolio2.setTitle("Protfolio #2");
-		
-		resp.getWriter().println(portfolio1.getHtmlString());
-		resp.getWriter().println(portfolio2.getHtmlString());
-		
-		for (int i = 0; i<portfolio1.getProtfolioSize()-1; i++){
-			portfolio1.getStocks()[i] = portfolio1.getStocks()[i+1];
-		}
-		portfolio1.setProtfolioSize(portfolio1.getProtfolioSize()-1);
-		
-		resp.getWriter().println(portfolio1.getHtmlString());
-		resp.getWriter().println(portfolio2.getHtmlString());
-		
-		portfolio2.getStocks()[portfolio2.getProtfolioSize()-1].setBid(55.55f);
-		resp.getWriter().println(portfolio1.getHtmlString());
-		resp.getWriter().println(portfolio2.getHtmlString());
-		*/
+	
 	}
 }
